aadea81625bd6c8fbb352f783153949b2f5a13e8
juan <jmflorezff@gmail.com>
2015-09-20 11:36:17 -0500

    - Agregada nueva lógica que reduce un poco el tiempo de ejecución

--- simulation.py
+++ simulation.py
@@ -31,7 +31,7 @@ class Agent:
         self.total_rewards = 0
         self.distance_traversed = 0
 
-        print('Agent beginning at {0}'.format(start_pos))
+        print('Agent beginning at %s' % start_pos)
         print()
 
     def perceive(self, requests):
@@ -42,22 +42,14 @@ class Agent:
 
             # Passengers
             options = [Destination(req, False) for req in self.passengers]
+            # Requests
             options += [Destination(req, True) for req in self.requests]
 
-            best_path = None
-            best_reward = float('-inf')
+            best_path = self._best_path(options)
 
-            for path in self._simulate_paths(options, self.current_passengers):
-                reward = self._calculate_reward(path)
+            self.destinations = best_path.to_list()
 
-                if reward > best_reward:
-                    best_reward = reward
-                    best_path = path
-
-            self.destinations = best_path
-
-            print('Best path determined:', best_path)
-                
+            print('Best path determined:', best_path , '\n')                
     def act(self):
         if self.destinations:
             dest = self.destinations[0]
@@ -76,7 +68,7 @@ class Agent:
                     self.pos.y += sign(dest.y - self.pos.y)
                 self.distance_traversed += 1
         else:
-            print('No requests. Idle.')
+            print('No requests. Idle.\n')
 
     @property
     def current_passengers(self):
@@ -109,6 +101,27 @@ class Agent:
 
         print('Reward: {0}'.format(request.reward))
         print()
+
+    def _best_path(self, options):
+        paths = Path([self.pos],
+                     None,
+                     self.current_passengers,
+                     options).extend()
+        min_cost = float('inf')
+        extended = True
+
+        while extended:
+            extended = False
+            for path in paths:
+                if path.can_extend and path.cost <= min_cost:
+                    extended = True
+                    paths.remove(path)
+                    paths += path.extend()
+                    paths = sorted(paths, key=lambda path: path.cost)
+                    min_cost = paths[0].cost
+                    break
+
+        return paths[0]
 
     def _simulate_paths(self, options, passengers):
         if not options:
@@ -192,11 +205,77 @@ class Destination(Point):
         self.is_pickup = is_pickup
         self.request = request
 
+    @property
+    def passengers(self):
+        return self.request.passengers
+
+    def destination(self):
+        if not self.is_pickup:
+            raise TypeError('This PathNode is already a destination')
+
+        return Destination(self.request, False)
+
+class Path:
+    def __init__(self,
+                 path,
+                 next_node,
+                 passengers,
+                 options,
+                 prev_cost=0):
+        # Can only be extended if there are options left
+        self.can_extend = bool(options)
+        self.options = options
+
+        # The amount of passengers for this simulated run
+        self.passengers = passengers
+
+        # Path is the route so far, next_node is the next step in the route
+        if next_node:
+            path += [next_node]
+            
+        self._path = path
+        
+        if not next_node:
+            self.cost = prev_cost
+        else:
+            self.cost = prev_cost + path[-1].dist(path[-2])
+
+    def extend(self):
+        new_paths = []
+        for i, option in enumerate(self.options):
+            cur_options = self.options[:]
+            cur_passengers = self.passengers
+            if option.is_pickup:
+                if option.passengers + cur_passengers <= MAX_PASSENGERS:
+                    cur_passengers += option.passengers
+                    cur_options.append(option.destination())
+                else:
+                    continue
+            else:
+                cur_passengers -= option.passengers
+                
+            other_opts = cur_options[:i] + cur_options[i+1:]
+            new_paths.append(Path(self._path[:],
+                                  option,
+                                  cur_passengers,
+                                  other_opts,
+                                  self.cost))
+        return new_paths
+
+    def to_list(self):
+        return self._path[1:]
+
+    def __repr__(self):
+        return '%s' % self._path
+
+    def __str__(self):
+        return self.__repr__()
+
 
 agent = Agent(Point(0,0))
 
 for _ in range(200):
-    if random.random() < 0.1 and len(agent.requests) <= 7:
+    if random.random() < 0.1 and len(agent.requests) <= 5:
         origin = Point(random.randint(0, 10), random.randint(0, 10))
         destination = Point(random.randint(0, 10), random.randint(0, 10))
         agent.perceive([Request(origin, destination, random.randint(1, 4))])
